ab4fdb321053b5f6c950f6dd8a3cba6942c7c996
Anna <119744825+AnnMarko@users.noreply.github.com>
2025-03-10 11:42:46 +0200

    DataManagerDots added

---
+++ DataManagerDots/Dot.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Dot
+    {
+        private double _x, _y, _z;
+
+        public double X
+        {
+            get { return _x; }
+            private set { _x = value; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            private set { _y = value; }
+        }
+
+        public double Z
+        {
+            get { return _z; }
+            private set { _z = value; }
+        }
+
+        public Dot(double x = 0, double y = 0, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void ShowCoordinates()
+        {
+            Console.WriteLine($"Координати: ( {X}  {Y}  {Z} )");
+            Console.WriteLine();
+        }
+
+        public double DistanceToOrigin()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public double DistanceToDot(Dot dot)
+        {
+            double xOffset = X - dot.X;
+            double yOffset = Y - dot.Y;
+            double zOffset = Z - dot.Z;
+            return Math.Sqrt(xOffset * xOffset + yOffset * yOffset + zOffset * zOffset);
+        }
+
+        public void ShowSphericalCoordinates()
+        {
+            double radius = DistanceToOrigin();
+            double theta = radius == 0 ? 0 : Math.Acos(Z / radius);
+            double phi = Math.Atan2(Y, X);
+
+            Console.WriteLine($"Сферичні координати:");
+            Console.WriteLine();
+
+            Console.WriteLine($"Радіус = {Math.Round(radius, 2)}");
+            Console.WriteLine($"Полярний кут = {Math.Round(theta, 2)} радіан, або {Math.Round(theta * 180 / Math.PI, 2)}°");
+            Console.WriteLine($"Азимутальний кут = {Math.Round(phi, 2)} радіан, або {Math.Round(phi * 180 / Math.PI, 2)}°");
+            Console.WriteLine();
+        }
+
+        public void ShowSymetricalDots()
+        {
+            Console.WriteLine($"Координати точок, симетричних до даної:");
+            Console.WriteLine();
+
+            Console.WriteLine($"- відносно площини XY: ( {X}  {Y}  {(Z != 0 ? -Z : 0)} )");
+            Console.WriteLine($"- відносно площини YZ: ( {(X != 0 ? -X : 0)}  {Y}  {Z} )");
+            Console.WriteLine($"- відносно площини XZ: ( {X}  {(Y != 0 ? -Y : 0)}  {Z} )");
+            Console.WriteLine();
+            Console.WriteLine($"- відносно осі X: ( {X}  {(Z != 0 ? -Z : 0)}  {(Z != 0 ? -Z : 0)} )");
+            Console.WriteLine($"- відносно осі Y: ( {(X != 0 ? -X : 0)}  {Y}  {(Z != 0 ? -Z : 0)} )");
+            Console.WriteLine($"- відносно осі Z: ( {(X != 0 ? -X : 0)}  {(Y != 0 ? -Y : 0)}  {Z} )");
+            Console.WriteLine();
+        }
+
+        public void Move(double x = 0, double y = 0, double z = 0)
+        {
+            if (x != 0)
+                X += x;
+            if (y != 0)
+                Y += y;
+            if (z != 0)
+                Z += z;
+        }
+
+        public void MoveX(double offset)
+        {
+            X += offset;
+        }
+
+        public void MoveY(double offset)
+        {
+            Y += offset;
+        }
+
+        public void MoveZ(double offset)
+        {
+            Z += offset;
+        }
+    }
+}
---
+++ DataManagerDots/Program.cs
@@ -0,0 +1,299 @@
+﻿/*
+    Створити клас точок у просторі, який містить закриті дані-члени координати
+    x, y і z, має перевантажені конструктори, включаючи конструктор за
+    замовчуванням (стандартний). Містить відкриті властивості для доступу до
+    закритих полів. Має методи переміщення точки по осях X, Y і Z, визначення
+    відстані до початку координат, відстані між двома точками, перетворення у
+    сферичні координати, знаходження координат точок, симетричних до заданої
+    відносно площин XY, YZ і XZ, осей X, Y і Z. У головній функції додатка
+    створювати об'єкти, в кількості необхідній користувачеві, за допомогою
+    різних конструкторів, і вивести на екран інформацію необхідну користувачеві.
+    Створені об'єкти зберегти у файлі після завершення роботи програми.
+*/
+
+using System.Text;
+using System.Text.Json;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            string options = @"1) Переміщення
+2) Визначити відстань до початку координат
+3) Визначити відстань між двома точками
+4) Перетворити координати у сферичні
+5) Знайти координати симетричних точок
+0) Назад
+
+Ваш вибір: ";
+
+            Dot[] loadedDots = null;
+            bool withLoadedDots = false;
+
+            if (File.Exists("dots.json"))
+            {
+                string jsonFromFile = File.ReadAllText("dots.json");
+                if (!String.IsNullOrEmpty(jsonFromFile))
+                {
+                    int loadDots;
+                    do
+                    {
+                        Console.Clear();
+                        PrettyInput("У Вас є збережені точки, бажаєте завантажити їх (1) або перезаписати (0)?: ", out loadDots);
+                    } while (loadDots < 0 || loadDots > 1);
+
+                    if (loadDots == 1)
+                    {
+                        loadedDots = JsonSerializer.Deserialize<Dot[]>(jsonFromFile);
+                    }
+                }
+            }
+            else
+            {
+                File.Create("dots.json").Close();
+            }
+
+            int extraSpace;
+            if (loadedDots == null || loadedDots.Length == 0)
+            {
+                extraSpace = 0;
+            }
+            else
+            {
+                extraSpace = loadedDots.Length;
+                withLoadedDots = true;
+            }
+
+            int dotsCount;
+            do
+            {
+                Console.Clear();
+                PrettyInput("Введіть кількість потрібних точок: ", out dotsCount);
+                if (withLoadedDots && dotsCount == 0)
+                {
+                    break;
+                }
+            } while (dotsCount <= 0);
+
+            dotsCount += extraSpace;
+
+            Dot[] dots = new Dot[dotsCount];
+
+            Console.Clear();
+
+            if (withLoadedDots)
+            {
+                for (int i = 0; i < extraSpace; i++)
+                {
+                    dots[i] = loadedDots[i];
+                }
+                ShowAllDots(dots);
+            }
+
+            for (int i = extraSpace; i < dotsCount; i++)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine($"Точка №{i + 1} з {dotsCount}");
+                Console.WriteLine("");
+
+                Console.WriteLine("Введіть координати (натисніть Enter для значення за замовчуванням)");
+                string x = PrettyInputWithEmptyString("X: ");
+                string y = PrettyInputWithEmptyString("Y: ");
+                string z = PrettyInputWithEmptyString("Z: ");
+
+                Dot dot = new Dot();
+                if (!String.IsNullOrEmpty(x))
+                    dot.MoveX(Convert.ToInt32(x));
+                if (!String.IsNullOrEmpty(y))
+                    dot.MoveY(Convert.ToInt32(y));
+                if (!String.IsNullOrEmpty(z))
+                    dot.MoveZ(Convert.ToInt32(z));
+
+                dots[i] = dot;
+
+                Console.Clear();
+                ShowAllDots(dots);
+            }
+
+            string json = JsonSerializer.Serialize(dots, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText("dots.json", json);
+
+            while (true)
+            {
+                int dotNumber;
+                do
+                {
+                    Console.Clear();
+                    ShowAllDots(dots);
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine();
+
+                    PrettyInput("Оберіть номер точки (0 - вийти з програми): ", out dotNumber);
+                } while (dotNumber < 0 || dotNumber > dotsCount);
+
+                if (dotNumber == 0)
+                {
+                    return;
+                }
+
+                dotNumber--;
+
+                Console.Clear();
+
+                DotManage(dots, dotNumber, options);
+            }
+        }
+
+        static void DotManage(Dot[] dots, int dotNumber, string options)
+        {
+            while (true)
+            {
+                Console.WriteLine("--------- Ваша точка ---------");
+                Console.WriteLine();
+                dots[dotNumber].ShowCoordinates();
+
+                int option;
+                do
+                {
+                    PrettyInput(options, out option, 7);
+                } while (option < 0 && option <= 5);
+
+                Console.Clear();
+                Console.WriteLine("--------- Ваша точка ---------");
+                Console.WriteLine();
+                dots[dotNumber].ShowCoordinates();
+
+                switch (option)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        MoveDot(dots[dotNumber]);
+                        string json = JsonSerializer.Serialize(dots, new JsonSerializerOptions
+                        {
+                            WriteIndented = true
+                        });
+
+                        File.WriteAllText("dots.json", json);
+                        break;
+                    case 2:
+                        Console.WriteLine($"Відстань до початку координат: {Math.Round(dots[dotNumber].DistanceToOrigin(), 2)}");
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        ShowDistanceToDot(dots, dotNumber);
+                        break;
+                    case 4:
+                        dots[dotNumber].ShowSphericalCoordinates();
+                        break;
+                    case 5:
+                        dots[dotNumber].ShowSymetricalDots();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("натисніть будь-яку клавішу...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void ShowDistanceToDot(Dot[] dots, int dotNumber)
+        {
+            int dotToCompareNumber;
+            do
+            {
+                Console.Clear();
+                ShowAllDots(dots);
+                PrettyInput("До якої точки знайти відстань?: ", out dotToCompareNumber);
+                dotToCompareNumber--;
+            } while (dotToCompareNumber < 0 || dotToCompareNumber >= dots.Length);
+
+            Console.Clear();
+
+            Console.WriteLine($"Точка {dotNumber + 1})");
+            dots[dotNumber].ShowCoordinates();
+
+            Console.WriteLine($"Точка {dotToCompareNumber + 1})");
+            dots[dotToCompareNumber].ShowCoordinates();
+
+            Console.WriteLine($"Відстань між ними: {Math.Round(dots[dotNumber].DistanceToDot(dots[dotToCompareNumber]), 2)}");
+            Console.WriteLine();
+        }
+        static void MoveDot(Dot dot)
+        {
+            Console.WriteLine("На скільки перемістити точку по кожній з координат? (натисніть Enter, якщо переміщення не потрібне)");
+            string x = PrettyInputWithEmptyString("X: ");
+            string y = PrettyInputWithEmptyString("Y: ");
+            string z = PrettyInputWithEmptyString("Z: ");
+
+            if (!String.IsNullOrEmpty(x))
+                dot.MoveX(Convert.ToInt32(x));
+            if (!String.IsNullOrEmpty(y))
+                dot.MoveY(Convert.ToInt32(y));
+            if (!String.IsNullOrEmpty(z))
+                dot.MoveZ(Convert.ToInt32(z));
+        }
+
+        static void ShowAllDots(Dot[] dots)
+        {
+            Console.WriteLine("--------- Всі точки ---------");
+            Console.WriteLine();
+            for (int i = 0; i < dots.Length; i++)
+            {
+                if (dots[i] == null)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Точок немає");
+                    }
+                    break;
+                }
+                Console.Write($"Точка {i + 1}) ");
+                dots[i].ShowCoordinates();
+            }
+        }
+
+        static void PrettyInput(string output, out int variable, int rows = 0)
+        {
+            Console.Write(output);
+            while (!int.TryParse(Console.ReadLine(), out variable))
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - (1 + rows));
+
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, Console.CursorTop);
+
+                Console.Write(output);
+            }
+        }
+
+        static string PrettyInputWithEmptyString(string output)
+        {
+            Console.Write(output);
+            string input = Console.ReadLine().Trim();
+            while (!String.IsNullOrEmpty(input) && !int.TryParse(input, out int number))
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, Console.CursorTop);
+
+                Console.Write(output);
+                input = Console.ReadLine().Trim();
+            }
+            return input;
+        }
+    }
+}
